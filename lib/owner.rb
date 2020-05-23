8913aa09231d2256bb1ce78133de8e05b4e5ef08
Richard Hanolt <rhanolt@gmail.com>
2020-05-23 18:25:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,9 +3,12 @@ require 'pry'
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @species = "human"
+    @@all = self
   end
 
   def say_species
