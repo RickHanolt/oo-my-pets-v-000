69b205ce9491f545ef3f4558b415aa782716b7fe
Richard Hanolt <rhanolt@gmail.com>
2020-05-23 18:21:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,9 @@
 class Owner
   attr_reader :name, :species
 
-  def initialize(name, species)
+  def initialize(name)
     @name = name
-    @species = species
+    @species = human
   end
-  
+
 end
